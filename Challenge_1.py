99d25abc869345df04d83c056f4ceab0d1edc4f7
Camilo Cordero <cami49@hotmail.com>
2023-12-04 13:34:58 -0500

    Commit

--- Challenge_1.py
+++ Challenge_1.py
@@ -1,28 +1,38 @@
-seguir = True
-datos = None
-resultado = 0
-while (seguir == True):
-        def ajustar_lista ():
-            global seguir
-            global resultado
-            datos = input("Input Data:")
-            if datos == "final":
-                seguir = False
-            else:
-                caracteres = [caracter for caracter in datos if caracter.isdigit()]
-                print(caracteres)
-                primer_numero = next((x for x in caracteres if x.isdigit()), None)
-                caracteres_al_reves = list(reversed(caracteres))
-                ultimo_numero = next((x for x in caracteres_al_reves if x.isdigit()), None)
-                resultado = resultado + int(f"{primer_numero}{ultimo_numero}")
-                print (resultado)
-            return None
-        ajustar_lista()
-    
+# Inicialización de variables
+seguir = True  # Variable de control del bucle
+datos = None  # Variable para almacenar los datos ingresados por el usuario
+resultado = 0  # Variable para almacenar el resultado acumulado
 
+# Bucle principal
+while seguir:  # Mientras 'seguir' sea True
+    def ajustar_lista():
+        # Indica que se utilizarán las variables globales 'seguir' y 'resultado'
+        global seguir
+        global resultado
 
-print ("Thanks for using :)")
+        # Captura de datos ingresados por el usuario
+        datos = input("Input Data:")
+        
+        # Verificación de si se ingresa 'final' para detener el bucle
+        if datos == "final":
+            seguir = False
+        else:
+            # Procesamiento de los datos ingresados para obtener números
+            caracteres = [caracter for caracter in datos if caracter.isdigit()]
+            print(caracteres)
+            
+            # Obtención del primer número y último número de la lista 'caracteres'
+            primer_numero = next((x for x in caracteres if x.isdigit()), None)
+            caracteres_al_reves = list(reversed(caracteres))
+            ultimo_numero = next((x for x in caracteres_al_reves if x.isdigit()), None)
+            
+            # Cálculo del resultado acumulado sumando los números obtenidos
+            resultado = resultado + int(f"{primer_numero}{ultimo_numero}")
+            print(resultado)
+        
+        return None
 
+    # Llamada a la función 'ajustar_lista()' en cada iteración del bucle
+    ajustar_lista()
 
-
-# Crear una lista con caracteres individuales del string
+print("Thanks for using :)")
